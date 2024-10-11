03e34d4e99b3e5a324e2bd032a1f5983e0300667
Joren <rothmanjoren@gmail.com>
2024-10-11 17:10:52 +0200

    Update

--- apps/server-old/src/logger.ts
+++
@@ -1,9 +0,0 @@
-import pino from "pino";
-
-export const logger = pino({});
-
-export const serverLogger = logger.child({ module: "server" });
-
-export const socketLogger = logger.child({ module: "socket" });
-
-export const redisLogger = logger.child({ module: "redis" });
